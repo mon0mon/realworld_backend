4c6cd5c5851575674c7d8ebf541ac9bf1571946e
이민기 <mon0mon@naver.com>
2024-09-09 21:25:18 +0900

    🎉 init : 신규 프로젝트 생성

---
+++ src/test/java/kr/neoventureholdings/realword_backend/RealwordBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.neoventureholdings.realword_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealwordBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
