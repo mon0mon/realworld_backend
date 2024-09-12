6233593c74063c66adac86db7cd1d69c448f5ba6
이민기 <mon0mon@naver.com>
2024-09-12 22:42:07 +0900

    ✨ feat : Authentication - Auth Controller 추가 (기본 요청 틀 추가)

--- src/main/java/kr/neoventureholdings/realword_backend/auth/controller/AuthController.java
+++ src/main/java/kr/neoventureholdings/realword_backend/auth/controller/AuthController.java
@@ -3,7 +3,11 @@ package kr.neoventureholdings.realword_backend.auth.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -12,5 +16,15 @@ public class AuthController {
   @PostMapping("/users")
   public ResponseEntity<String> registerUser() {
     return new ResponseEntity<>("OK", HttpStatus.OK);
+  }
+
+  @GetMapping("/users")
+  public ResponseEntity<UserDetails> getCurrentUser(@AuthenticationPrincipal UserDetails userDetails) {
+    return new ResponseEntity<>(userDetails, HttpStatus.OK);
+  }
+
+  @PutMapping("/user")
+  public ResponseEntity<UserDetails> updateUser(@AuthenticationPrincipal UserDetails userDetails) {
+    return new ResponseEntity<>(userDetails, HttpStatus.OK);
   }
 }
