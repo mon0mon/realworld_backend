144dd5bfb65c634aae69e0adc7a0153d6b2d48fb
이민기 <mon0mon@naver.com>
2024-09-12 22:32:24 +0900

    ✨ feat : Authentication - User Request, Response Dto 추가

---
+++ src/main/java/kr/neoventureholdings/realword_backend/auth/dto/UserRegisterRequestDto.java
@@ -0,0 +1,28 @@
+package kr.neoventureholdings.realword_backend.auth.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserRegisterRequestDto {
+  @Email
+  @NotEmpty
+  private String email;
+  @Size(min = 5, max = 20)
+  @NotEmpty
+  private String username;
+  @NotEmpty
+  private String password;
+}
