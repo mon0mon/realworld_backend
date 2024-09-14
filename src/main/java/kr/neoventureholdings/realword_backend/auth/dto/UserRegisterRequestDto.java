f06202ecf96cfb6c5f4dc1ee094c48ede2a6e13c
이민기 <mon0mon@naver.com>
2024-09-14 22:16:27 +0900

    ✨ feat : User 도메인 수정 - User Entity 항목 수정 - User Dto 항목 추가 및 수정

--- src/main/java/kr/neoventureholdings/realword_backend/auth/dto/UserRegisterRequestDto.java
+++ src/main/java/kr/neoventureholdings/realword_backend/auth/dto/UserRegisterRequestDto.java
@@ -3,12 +3,14 @@ package kr.neoventureholdings.realword_backend.auth.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
+import kr.neoventureholdings.realword_backend.auth.domains.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Getter
 @Setter
@@ -25,4 +27,12 @@ public class UserRegisterRequestDto {
   private String username;
   @NotEmpty
   private String password;
+
+  public User toUser(PasswordEncoder passwordEncoder) {
+    return User.builder()
+        .username(this.username)
+        .password(passwordEncoder.encode(this.password))
+        .email(this.email)
+        .build();
+  }
 }
