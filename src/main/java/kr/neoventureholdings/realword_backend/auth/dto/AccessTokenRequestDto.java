ef93dc077546fe89abc83334050f8824ae735b5b
이민기 <mon0mon@naver.com>
2024-09-14 22:13:49 +0900

    ✨ feat : JWT 토큰 DTO 추가

---
+++ src/main/java/kr/neoventureholdings/realword_backend/auth/dto/AccessTokenRequestDto.java
@@ -0,0 +1,21 @@
+package kr.neoventureholdings.realword_backend.auth.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class AccessTokenRequestDto {
+  @NotEmpty
+  private String token;
+  @NotNull
+  private Integer expiredTime;
+}
---
+++ src/main/java/kr/neoventureholdings/realword_backend/auth/dto/AccessTokenResponseDto.java
@@ -0,0 +1,21 @@
+package kr.neoventureholdings.realword_backend.auth.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AccessTokenResponseDto {
+  @NotEmpty
+  private String token;
+  @NotNull
+  private Integer expiresTime;
+}
