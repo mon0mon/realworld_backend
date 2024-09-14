4703b22a3a542917bc8e03d66c3dd91a9c8db348
이민기 <mon0mon@naver.com>
2024-09-14 22:18:48 +0900

    ✨ feat : Spring Security UserDetail 작업 - CustomUserDetail 추가 - CustomUserDetailService 추가

---
+++ src/main/java/kr/neoventureholdings/realword_backend/config/security/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package kr.neoventureholdings.realword_backend.config.security;
+
+import kr.neoventureholdings.realword_backend.auth.domains.User;
+import kr.neoventureholdings.realword_backend.auth.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+  private final UserRepository userRepository;
+
+  @Override
+  public CustomUserDetail loadUserByUsername(String username)
+      throws UsernameNotFoundException {
+    User user = userRepository
+        .findByUsername(username)
+        .orElseThrow(() -> new UsernameNotFoundException("No Match User"));
+    return CustomUserDetail.of(user);
+  }
+}
