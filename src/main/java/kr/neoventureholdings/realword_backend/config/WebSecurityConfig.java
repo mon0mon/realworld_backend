0b00fffe77b753fcc9fc0a8f845a7fc3dd90261f
이민기 <mon0mon@naver.com>
2024-09-14 22:10:43 +0900

    🛠️ project : JPA 세팅 - Spring Security로 인해 H2 Database 연결에 문제가 있던 부분 수정

--- src/main/java/kr/neoventureholdings/realword_backend/config/WebSecurityConfig.java
+++ src/main/java/kr/neoventureholdings/realword_backend/config/WebSecurityConfig.java
@@ -1,50 +1,84 @@
 package kr.neoventureholdings.realword_backend.config;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfig {
+  private static final String[] PERMIT_ALL_PATTERNS = new String[] {
+      "/swagger-ui/**",
+      "/h2-console",
+      "/h2-console/**",
+      "/users/*",
+      "/users/**",
+      "/**"
+  };
+  private final PasswordEncoder passwordEncoder;
 
   @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.authorizeHttpRequests((authorizationRequest) ->
-            authorizationRequest
-                .anyRequest().authenticated()
-        )
-        .formLogin((formLogin) ->
-            formLogin
-                .usernameParameter("username")
-                .passwordParameter("password")
-                .defaultSuccessUrl("/", true)
-        );
+  public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+    httpSecurity.authorizeHttpRequests(
+        authHttp -> authHttp
+            .requestMatchers( //인증 관련 정보만 추가
+                Stream.of(PERMIT_ALL_PATTERNS)
+                    .map(AntPathRequestMatcher::antMatcher)
+                    .toArray(AntPathRequestMatcher[]::new)
+            )
+            .permitAll()
+            .anyRequest()
+            .authenticated()
+    );
 
-    return http.build();
+    httpSecurity.csrf(httpSecurityCsrfConfigurer ->
+        httpSecurityCsrfConfigurer
+            .ignoringRequestMatchers("/h2-console/**"));
+
+    httpSecurity.headers(httpSecurityHeadersConfigurer ->
+        httpSecurityHeadersConfigurer.frameOptions(
+            FrameOptionsConfig::disable
+        ));
+
+    //  FormLogin 및 Logout 비활성화
+    httpSecurity
+        .formLogin(AbstractHttpConfigurer::disable)
+        .logout(AbstractHttpConfigurer::disable)
+    ;
+
+    //  Session 유지 방식을 Stateless 방식으로 변경
+    httpSecurity.sessionManagement(
+        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+    return httpSecurity.build();
   }
 
   @Bean
-  public UserDetailsService userDetailsService() {
-    InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-    manager.createUser(
-        User.withUsername("user1")
-            .password(passwordEncoder().encode("1234")
-            ).build());
-    return manager;
-  }
+  CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(List.of("*"));
+    configuration.setAllowedMethods(
+        Arrays.asList("HEAD", "POST", "GET", "DELETE", "PUT", "PATCH", "OPTION"));
+    configuration.setAllowedHeaders(List.of("*"));
+    configuration.setAllowCredentials(true);
 
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 }
