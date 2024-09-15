8f77407caec738a26e4e2091c9114636968af8b5
이민기 <mon0mon@naver.com>
2024-09-15 23:00:03 +0900

    ✨ feat : Exception Handling - Global Exception Hanlder 추가 - 요구사항의 오류 응답 값 형식에 맞게끔 리턴하도록 수정

--- src/main/java/kr/neoventureholdings/realword_backend/exception/ErrorResponse.java
+++ src/main/java/kr/neoventureholdings/realword_backend/exception/ErrorResponse.java
@@ -1,53 +1,58 @@
 package kr.neoventureholdings.realword_backend.exception;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 
 @Getter
-@NoArgsConstructor
+@AllArgsConstructor
 public class ErrorResponse {
+  @JsonIgnore
+  private HttpStatus status;
+  private List<String> body = new ArrayList<>();
 
-  private int status;
-  private List<FieldError> errors;
-
-  private ErrorResponse(final HttpStatus statusCode, final List<FieldError> errors) {
-    this.status = statusCode.value();
-    this.errors = errors;
+  public ErrorResponse(HttpStatus httpStatus) {
+    this.status = httpStatus;
   }
 
-  public static ErrorResponse of(final HttpStatus statusCode, final BindingResult bindingResult) {
-    return new ErrorResponse(statusCode, FieldError.of(bindingResult));
+  public ErrorResponse(HttpStatus httpStatus, BindingResult bindingResult) {
+    this.status = httpStatus;
+    this.body = extractBindingResults(bindingResult);
   }
 
-  public static ErrorResponse of(final HttpStatus statusCode, final List<FieldError> errors) {
-    return new ErrorResponse(statusCode, errors);
+  public void setBody(String message) {
+    this.body = new ArrayList<>(List.of(message));
   }
 
-  @Getter
-  @NoArgsConstructor
-  public static class FieldError {
+  public void setBody(List<String> body) {
+    this.body = body;
+  }
 
-    private String reason;
+  public void addBody(String message) {
+    this.body.add(message);
+  }
 
-    private FieldError(final String reason) {
-      this.reason = reason;
-    }
+  public void addBody(List<String> body) {
+    this.body.addAll(body);
+  }
 
-    public static List<FieldError> of(final String reason) {
-      List<FieldError> fieldErrors = new ArrayList<>();
-      fieldErrors.add(new FieldError(reason));
-      return fieldErrors;
-    }
+  public void setBody(BindingResult bindingResult) {
+    this.body = extractBindingResults(bindingResult);
+  }
 
-    private static List<FieldError> of(final BindingResult bindingResult) {
-      final List<org.springframework.validation.FieldError> fieldErrors = bindingResult.getFieldErrors();
-      return fieldErrors.stream()
-          .map(error -> new FieldError(error.getDefaultMessage()))
-          .toList();
-    }
+  public void addBody(BindingResult bindingResult) {
+    this.body.addAll(extractBindingResults(bindingResult)
+    );
+  }
+
+  private static List<String> extractBindingResults(BindingResult bindingResult) {
+    return bindingResult.getFieldErrors()
+        .stream()
+        .map(fieldError -> fieldError.getField() + " : " + fieldError.getDefaultMessage())
+        .toList();
   }
 }
---
+++ src/main/java/kr/neoventureholdings/realword_backend/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package kr.neoventureholdings.realword_backend.exception.handler;
+
+import java.util.List;
+import kr.neoventureholdings.realword_backend.exception.ErrorResponse;
+import kr.neoventureholdings.realword_backend.exception.auth.AuthException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ErrorResponse> validationException(MethodArgumentNotValidException e) {
+    log.error("ValidationException", e);
+    ErrorResponse response = new ErrorResponse(HttpStatus.UNPROCESSABLE_ENTITY,
+        e.getBindingResult());
+    return new ResponseEntity<>(response, response.getStatus());
+  }
+
+  @ExceptionHandler(AuthException.class)
+  public ResponseEntity<ErrorResponse> handleAuthException(AuthException e) {
+    log.error("Auth Exception", e);
+    ErrorResponse response = new ErrorResponse(HttpStatus.BAD_REQUEST,
+        List.of(e.getMessage()));
+    return new ResponseEntity<>(response, response.getStatus());
+  }
+}
