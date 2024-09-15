1e24eddc19f3f3f8153eadf97e4c7761c1d4e8be
이민기 <mon0mon@naver.com>
2024-09-15 17:36:21 +0900

    ✨ feat : Spring Security UserDetailsService - CustomUserDetailsService에서 AccessToken 값으로 인증처리가 가능하도록 메소드 추가

--- src/main/java/kr/neoventureholdings/realword_backend/config/security/CustomUserDetailsService.java
+++ src/main/java/kr/neoventureholdings/realword_backend/config/security/CustomUserDetailsService.java
@@ -15,9 +15,15 @@ public class CustomUserDetailsService implements UserDetailsService {
 
   private final UserRepository userRepository;
 
+  public CustomUserDetail loadUserById(Long userId) throws UsernameNotFoundException {
+    User user = userRepository
+        .findById(userId)
+        .orElseThrow(() -> new UsernameNotFoundException("No Match User"));
+    return CustomUserDetail.of(user);
+  }
+
   @Override
-  public CustomUserDetail loadUserByUsername(String username)
-      throws UsernameNotFoundException {
+  public CustomUserDetail loadUserByUsername(String username) throws UsernameNotFoundException {
     User user = userRepository
         .findByUsername(username)
         .orElseThrow(() -> new UsernameNotFoundException("No Match User"));
