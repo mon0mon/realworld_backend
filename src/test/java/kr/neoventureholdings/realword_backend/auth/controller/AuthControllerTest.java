fb18cc7a6e1929cd95d898e1dd67381951074f10
이민기 <mon0mon@naver.com>
2024-09-15 18:09:05 +0900

    🧪 test : Auth Controller - Spring Boot Test로 변경 - POST /users/login 로그인 로직 테스트 ✅

--- src/test/java/kr/neoventureholdings/realword_backend/auth/controller/AuthControllerTest.java
+++ src/test/java/kr/neoventureholdings/realword_backend/auth/controller/AuthControllerTest.java
@@ -1,36 +1,69 @@
 package kr.neoventureholdings.realword_backend.auth.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import kr.neoventureholdings.realword_backend.auth.domains.User;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 class AuthControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+  private ObjectMapper objectMapper = new ObjectMapper();
   User user;
 
   @BeforeEach
   void beforeEach() {
     user = User.builder()
-        .email("test@examle.com")
-        .username("testUser")
-        .password("1234")
+        .email("user1@examle.com")
+        .username("user1")
+        .password("pass")
         .build();
   }
 
   @Test
   @DisplayName("사용자 인증 컨트롤러 테스트 - 로그인")
   void loginTest() {
-
+    Assertions.catchThrowable(
+        () -> mockMvc.perform(post("/users/login")
+                .content(objectMapper.writeValueAsString(user))
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+            )
+            .andDo(print())
+            .andExpect(status().isOk())
+    );
   }
 
   @Test
   @DisplayName("사용자 인증 컨트롤러 테스트 - 회원가입")
   void registerTest() {
+    user = User.builder()
+        .email("user2@example.com")
+        .username("user2")
+        .password("pass")
+        .build();
 
+    Assertions.catchThrowable(
+        () -> mockMvc.perform(post("/users")
+                .content(objectMapper.writeValueAsString(user))
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+            )
+            .andDo(print())
+            .andExpect(status().isOk())
+    );
   }
 
   @Test
