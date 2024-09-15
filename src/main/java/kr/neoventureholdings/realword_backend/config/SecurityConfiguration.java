bdd16a4b2e6102414171cd61a4a99100817a1132
이민기 <mon0mon@naver.com>
2024-09-15 17:54:53 +0900

    ✨ feat : Spring Security - Authorization Filter에서 JWT Token으로 인증처리가 되도록 수정 - Spring Security 설정에서 인증필터를 사용하도록 추가 - Spring Security 설정에서 인증을 거치지 않는 URL 매핑 수정 - CSRF 비활성화 (H2 Database Console 접속 간 에러 수정)

---
+++ src/main/java/kr/neoventureholdings/realword_backend/config/SecurityConfiguration.java
@@ -0,0 +1,90 @@
+package kr.neoventureholdings.realword_backend.config;
+
+import java.util.Arrays;
+import java.util.List;
+import kr.neoventureholdings.realword_backend.config.security.CustomAuthenticationProvider;
+import kr.neoventureholdings.realword_backend.config.security.CustomAuthorizationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+  private final CustomAuthenticationProvider authProvider;
+
+  @Bean
+  public AuthenticationManager authManager(HttpSecurity httpSecurity) throws Exception {
+    AuthenticationManagerBuilder authenticationManagerBuilder = httpSecurity.getSharedObject(
+        AuthenticationManagerBuilder.class);
+    authenticationManagerBuilder.authenticationProvider(authProvider);
+    return authenticationManagerBuilder.build();
+  }
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity httpSecurity,
+      CustomAuthorizationFilter customAuthorizationFilter) throws Exception {
+    httpSecurity.authorizeHttpRequests((authz) ->
+        authz
+            .requestMatchers("/h2-console").permitAll()
+            .requestMatchers("/users/login").permitAll()
+            .requestMatchers("/users").permitAll()
+            .anyRequest().authenticated());
+
+    httpSecurity.csrf(AbstractHttpConfigurer::disable);
+
+    httpSecurity.headers(httpSecurityHeadersConfigurer ->
+        httpSecurityHeadersConfigurer.frameOptions(
+            FrameOptionsConfig::disable
+        ));
+
+    //  FormLogin 및 Logout 비활성화
+    httpSecurity
+        .formLogin(AbstractHttpConfigurer::disable)
+        .logout(AbstractHttpConfigurer::disable)
+    ;
+
+    //  Session 유지 방식을 Stateless 방식으로 변경
+    httpSecurity.sessionManagement(
+        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+    //  CORS 설정 추가
+    httpSecurity.cors(Customizer.withDefaults());
+
+    //  인증 필터 추가
+    httpSecurity.addFilterBefore(customAuthorizationFilter,
+        UsernamePasswordAuthenticationFilter.class);
+
+    return httpSecurity.build();
+  }
+
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(List.of("http://localhost:8080"));
+    configuration.setAllowedMethods(
+        Arrays.asList("HEAD", "POST", "GET", "DELETE", "PUT", "PATCH", "OPTIONS"));
+    configuration.setAllowedHeaders(List.of("*"));
+    configuration.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+  }
+}
