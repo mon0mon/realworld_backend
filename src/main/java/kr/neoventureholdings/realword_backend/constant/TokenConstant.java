f3fb6a99558a8e3157b16ed9f2a4b200db21cc7f
이민기 <mon0mon@naver.com>
2024-09-16 10:50:29 +0900

    ✨ feat : Spring Security Authorization - CustomAuthorizationFilter에서 Token 인증정보가 없을 경우 401을 리턴하도록 수정 - CustomAuthorizationFilter에서 h2-console에 대해서는 인증정보 검사를 하지 않도록 수정
    ♻️ refactor : Constant
    - TokenConstant를 추가하여, 전역에서 상수값을 사용할 수 있도록 수정

---
+++ src/main/java/kr/neoventureholdings/realword_backend/constant/TokenConstant.java
@@ -0,0 +1,10 @@
+package kr.neoventureholdings.realword_backend.constant;
+
+public class TokenConstant {
+  public static final String ACCESS_TOKEN = "access_token";
+  public static final String REFRESH_TOKEN = "refresh_token";
+  public static final String TOKEN_HEADER_PREFIX = "Token ";
+
+  private TokenConstant() {
+  }
+}
