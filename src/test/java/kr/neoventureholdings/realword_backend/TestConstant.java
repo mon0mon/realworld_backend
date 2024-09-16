ae0b2a0495f78bb07090f100292d8f0383910761
이민기 <mon0mon@naver.com>
2024-09-16 14:33:45 +0900

    🧪 test : Auth Test - Auth Controller Test에서 사용될 편의성 메소드 및 상수 추가

---
+++ src/test/java/kr/neoventureholdings/realword_backend/TestConstant.java
@@ -0,0 +1,15 @@
+package kr.neoventureholdings.realword_backend;
+
+public class TestConstant {
+  public static final String EMAIL = "user1@example.com";
+  public static final String USERNAME = "user1";
+  public static final String PASSWORD = "123";
+  public static final String REGISTER_EMAIL = "user2@example.com";
+  public static final String REGISTER_USERNAME = "user2";
+  public static final String REGISTER_PASSWORD = "123";
+  public static final String UPDATE_EMAIL = "user1_change@example.com";
+  public static final String UPDATE_USERNAME = "user1_change";
+  public static final String UPDATE_PASSWORD = "qwe";
+  public static final String UPDATE_BIO = "Hello World!";
+  public static final String UPDATE_IMAGE = "https://static-00.iconduck.com/assets.00/person-icon-2048x2048-wiaps1jt.png";
+}
