fea94afc68c46811f4b28147164dc4d122ea1e56
이민기 <mon0mon@naver.com>
2024-09-16 14:32:50 +0900

    ♻️ refactor : Common Dto - 생성자 옵션 추가

--- src/main/java/kr/neoventureholdings/realword_backend/common/dto/CommonResponseDto.java
+++ src/main/java/kr/neoventureholdings/realword_backend/common/dto/CommonResponseDto.java
@@ -1,16 +1,21 @@
 package kr.neoventureholdings.realword_backend.common.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import kr.neoventureholdings.realword_backend.auth.dto.UserResponseDto;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonInclude(Include.NON_NULL)
 public class CommonResponseDto {
   @JsonProperty("user")
