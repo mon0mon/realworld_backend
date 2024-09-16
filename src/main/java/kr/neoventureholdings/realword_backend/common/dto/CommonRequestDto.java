2a6ac2f53b0286e50b4e0673b982b5ac3e4a19bb
이민기 <mon0mon@naver.com>
2024-09-16 14:32:50 +0900

    ♻️ refactor : Common Dto - 생성자 옵션 추가

--- src/main/java/kr/neoventureholdings/realword_backend/common/dto/CommonRequestDto.java
+++ src/main/java/kr/neoventureholdings/realword_backend/common/dto/CommonRequestDto.java
@@ -2,11 +2,17 @@ package kr.neoventureholdings.realword_backend.common.dto;
 
 import jakarta.validation.Valid;
 import kr.neoventureholdings.realword_backend.auth.dto.UserRequestDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CommonRequestDto {
   @Valid
   private UserRequestDto user;
