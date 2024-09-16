0b7ab9cd1af226c183c5c5062c9e1d1c2d52dbdb
이민기 <mon0mon@naver.com>
2024-09-16 16:35:57 +0900

    ✨ feat : Profile DB - Profile Entity에 값 추가

--- src/main/java/kr/neoventureholdings/realword_backend/auth/domains/Profile.java
+++ src/main/java/kr/neoventureholdings/realword_backend/auth/domains/Profile.java
@@ -1,5 +1,6 @@
 package kr.neoventureholdings.realword_backend.auth.domains;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,4 +19,8 @@ public class Profile {
   @NotNull
   private User user;
   private boolean isFollowing;
+  @NotBlank
+  private String username;
+  private String bio;
+  private String image;
 }
