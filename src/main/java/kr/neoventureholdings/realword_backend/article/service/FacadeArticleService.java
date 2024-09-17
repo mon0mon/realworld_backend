f46751d4404fd8bf2244c3ab83c650fbfd108afd
이민기 <mon0mon@naver.com>
2024-09-17 23:47:47 +0900

    ✨ feat : Article - ArticleService에 비즈니스 로직 추가 - 비즈니스 로직에 Query Method 추가 - Controller에서 단일 조회 항목 추가
    ♻️ refactor : Article
    - Controller에서 Parameter를 단일 Dto로 받도록 수정

--- src/main/java/kr/neoventureholdings/realword_backend/article/service/FacadeArticleService.java
+++ src/main/java/kr/neoventureholdings/realword_backend/article/service/FacadeArticleService.java
@@ -1,5 +1,10 @@
 package kr.neoventureholdings.realword_backend.article.service;
 
+import java.util.List;
+import kr.neoventureholdings.realword_backend.article.domains.Article;
+import kr.neoventureholdings.realword_backend.article.dto.ArticleRequestDto;
+import kr.neoventureholdings.realword_backend.article.dto.ArticleRequestParamDto;
+import kr.neoventureholdings.realword_backend.config.security.authentication.CustomUserDetail;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -9,4 +14,24 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class FacadeArticleService {
   private final ArticleService articleService;
+
+  public Article getArticle(String slug) {
+    return articleService.getArticle(slug);
+  }
+
+  public List<Article> getArticles(ArticleRequestParamDto paramDto) {
+    return articleService.getArticles(paramDto);
+  }
+
+  public Article saveArticle(ArticleRequestDto articleRequestDto, CustomUserDetail userDetail) {
+    return articleService.saveArticle(articleRequestDto, userDetail);
+  }
+
+  public Article updateArticle(ArticleRequestDto articleRequestDto, String slug, CustomUserDetail userDetail) {
+    return articleService.updateArticle(articleRequestDto, slug, userDetail);
+  }
+
+  public boolean deleteArticle(String slug, CustomUserDetail userDetail) {
+    return articleService.deleteArticle(slug, userDetail);
+  }
 }
