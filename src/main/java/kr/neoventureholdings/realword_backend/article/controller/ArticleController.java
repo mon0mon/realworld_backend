102f556952dfa8a9cf76418d5e3f7e7c55352171
이민기 <mon0mon@naver.com>
2024-09-17 23:47:47 +0900

    ✨ feat : Article - ArticleService에 비즈니스 로직 추가 - 비즈니스 로직에 Query Method 추가 - Controller에서 단일 조회 항목 추가
    ♻️ refactor : Article
    - Controller에서 Parameter를 단일 Dto로 받도록 수정

--- src/main/java/kr/neoventureholdings/realword_backend/article/controller/ArticleController.java
+++ src/main/java/kr/neoventureholdings/realword_backend/article/controller/ArticleController.java
@@ -1,20 +1,24 @@
 package kr.neoventureholdings.realword_backend.article.controller;
 
+import kr.neoventureholdings.realword_backend.article.domains.Article;
+import kr.neoventureholdings.realword_backend.article.dto.ArticleRequestParamDto;
 import kr.neoventureholdings.realword_backend.article.service.FacadeArticleService;
 import kr.neoventureholdings.realword_backend.common.dto.CommonRequestDto;
 import kr.neoventureholdings.realword_backend.common.dto.CommonResponseDto;
+import kr.neoventureholdings.realword_backend.config.security.authentication.CustomUserDetail;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -22,32 +26,49 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/articles")
 public class ArticleController {
+
   private final FacadeArticleService articleService;
 
   @GetMapping
-  public ResponseEntity<CommonResponseDto> getArticles(
-      @RequestParam("author") String author,
-      @RequestParam("favorited") String favoritedByUsername,
-      @RequestParam("tag") String tag,
-      @RequestParam(name = "limit", value = "20") Integer limit,
-      @RequestParam(name = "offset", value = "0") Integer offset
-  ) {
+  public ResponseEntity<CommonResponseDto> getArticle(
+      @ModelAttribute ArticleRequestParamDto paramDto) {
     return ResponseEntity
         .ok()
         .body(CommonResponseDto
             .builder()
+            .articleResponseDtoList(articleService.getArticles(paramDto)
+                .stream()
+                .map(Article::to)
+                .toList()
+            )
+            .build()
+        );
+  }
+
+  @GetMapping("/{slug}")
+  public ResponseEntity<CommonResponseDto> getArticles(@PathVariable("slug") String slug) {
+    return ResponseEntity
+        .ok()
+        .body(CommonResponseDto
+            .builder()
+            .articleResponseDto(articleService.getArticle(slug).to())
             .build()
         );
   }
 
   @PostMapping
   public ResponseEntity<CommonResponseDto> saveArticle(
-      @Validated @RequestBody CommonRequestDto commonRequestDto
+      @Validated @RequestBody CommonRequestDto commonRequestDto,
+      @AuthenticationPrincipal CustomUserDetail userDetail
   ) {
     return ResponseEntity
         .ok()
         .body(CommonResponseDto
             .builder()
+            .articleResponseDto(
+                articleService.saveArticle(commonRequestDto.getArticle(), userDetail)
+                    .to()
+            )
             .build()
         );
   }
@@ -55,26 +76,27 @@ public class ArticleController {
   @PutMapping("/{slug}")
   public ResponseEntity<CommonResponseDto> updateArticle(
       @Validated @RequestBody CommonRequestDto commonRequestDto,
-      @PathVariable("slug") String slug
+      @PathVariable("slug") String slug,
+      @AuthenticationPrincipal CustomUserDetail userDetail
   ) {
     return ResponseEntity
         .ok()
         .body(CommonResponseDto
             .builder()
+            .articleResponseDto(
+                articleService.updateArticle(commonRequestDto.getArticle(), slug, userDetail)
+                    .to()
+            )
             .build()
         );
   }
 
   @DeleteMapping("/{slug}")
   public ResponseEntity<CommonResponseDto> deleteArticle(
-      @Validated @RequestBody CommonRequestDto commonRequestDto,
-      @PathVariable("slug") String slug
+      @PathVariable("slug") String slug,
+      @AuthenticationPrincipal CustomUserDetail userDetail
   ) {
-    return ResponseEntity
-        .ok()
-        .body(CommonResponseDto
-            .builder()
-            .build()
-        );
+    articleService.deleteArticle(slug, userDetail);
+    return ResponseEntity.ok(null);
   }
 }
