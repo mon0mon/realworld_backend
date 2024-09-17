f9ba9ede1ced164f1a3c2e9301eaa95de85bd456
이민기 <mon0mon@naver.com>
2024-09-17 09:54:40 +0900

    🛠️ project : Initial Data - 초기 데이터 값 수정 - HttpClient에서 사용 될 profile 변수 추가

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -3,4 +3,11 @@ INSERT INTO users (email, password)
 VALUES ('user1@example.com', '123');
 
 INSERT INTO profile (user_id, username)
-VALUES (1, 'user1');
+VALUES ((select id from users where email like 'user1@example.com'), 'user1');
+
+-- Profile 조회용 유저 정보 추가
+INSERT INTO users (email, password)
+VALUES ('celeb@example.com', '123');
+
+INSERT INTO profile (user_id, username)
+VALUES ((select id from users where email like 'celeb@example.com'), 'celeb');
