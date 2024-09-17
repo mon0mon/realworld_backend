56a59a87a9cfd38fe7cdecdfbacd1ce5101e3dce
이민기 <mon0mon@naver.com>
2024-09-17 20:41:02 +0900

    ✨ feat : Article - Article Response Dto 추가 - CommonResponseDto에 Article Response Dto 추가

---
+++ src/main/java/kr/neoventureholdings/realword_backend/article/dto/ArticleResponseDto.java
@@ -0,0 +1,30 @@
+package kr.neoventureholdings.realword_backend.article.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.LocalDateTime;
+import java.util.List;
+import kr.neoventureholdings.realword_backend.auth.dto.UserResponseDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ArticleResponseDto {
+  private String slug;
+  private String title;
+  private String description;
+  private String body;
+  private LocalDateTime createAt;
+  private LocalDateTime updatedAt;
+  private Boolean favorited;
+  private Integer facoritesCount;
+  @JsonProperty("author")
+  private UserResponseDto author;
+  private List<String> tags;
+}
