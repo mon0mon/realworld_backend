62458738937e56056f1229083c217f9ae7569f44
이민기 <mon0mon@naver.com>
2024-09-17 20:21:02 +0900

    🧪 test : Auth - User domain의 변경사항에 맞게끔 Domain Test 코드 수정 - User Mocking으로 테스트 코드 간소화

--- src/test/java/kr/neoventureholdings/realword_backend/auth/controller/AuthControllerTest.java
+++ src/test/java/kr/neoventureholdings/realword_backend/auth/controller/AuthControllerTest.java
@@ -12,8 +12,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import kr.neoventureholdings.realword_backend.TestConstant;
+import kr.neoventureholdings.realword_backend.config.security.authentication.CustomUserDetail;
 import kr.neoventureholdings.realword_backend.constant.TokenConstant;
 import kr.neoventureholdings.realword_backend.util.AuthTestUtils;
+import kr.neoventureholdings.realword_backend.util.TestUserDetailService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -23,11 +25,17 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 class AuthControllerTest {
 
   @Autowired
@@ -71,22 +79,19 @@ class AuthControllerTest {
 
   @Test
   @DisplayName("사용자 인증 컨트롤러 테스트 - 현재 로그인한 사용자 정보")
+  @PreAuthorize("isAuthenticated()")
+  @WithUserDetails(value = TestConstant.EMAIL, userDetailsServiceBeanName = "testUserDetailService")
   void getCurrentLoginUserTest() {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    CustomUserDetail userDetails = (CustomUserDetail) authentication.getPrincipal();
+
+    String token = userDetails.getToken();
+
     try {
-      mockMvc.perform(post("/users/login")
-              .content(objectMapper.writeValueAsString(AuthTestUtils.getLoginDefaultUserRequest()))
-              .contentType(MediaType.APPLICATION_JSON_VALUE)
+      mockMvc.perform(get("/user")
+          .header(HttpHeaders.AUTHORIZATION, TokenConstant.TOKEN_HEADER_PREFIX + token)
+          .contentType(MediaType.APPLICATION_JSON_VALUE)
           )
-          .andDo(print())
-          .andExpect(status().isOk())
-          .andDo(mvcResult -> {
-            String token = AuthTestUtils.getResponseToken(mvcResult.getResponse().getContentAsString());
-            System.out.println("token = " + token);
-            mockMvc.perform(get("/user")
-                .header(HttpHeaders.AUTHORIZATION, TokenConstant.TOKEN_HEADER_PREFIX + token)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-            );
-          })
           .andDo(print())
           .andDo(log())
           .andExpectAll(
@@ -104,22 +109,14 @@ class AuthControllerTest {
 
   @Test
   @DisplayName("사용자 인증 컨트롤러 테스트 - 사용자 정보 수정")
+  @PreAuthorize("isAuthenticated()")
+  @WithUserDetails(value = TestConstant.EMAIL, userDetailsServiceBeanName = "testUserDetailService")
   void updateUserInfoTest() {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    CustomUserDetail userDetails = (CustomUserDetail) authentication.getPrincipal();
+
+    String token = userDetails.getToken();
     try {
-      // 로그인 요청
-      MvcResult loginResult = mockMvc
-          .perform(post("/users/login")
-              .content(objectMapper.writeValueAsString(AuthTestUtils.getLoginDefaultUserRequest()))
-              .contentType(MediaType.APPLICATION_JSON_VALUE)
-          )
-          .andDo(print())
-          .andExpect(status().isOk())
-          .andReturn();  // 결과를 반환하여 이후에 사용할 수 있도록
-
-      // 응답에서 토큰 추출
-      String token = AuthTestUtils.getResponseToken(loginResult.getResponse().getContentAsString());
-      System.out.println("token = " + token);
-
       // 사용자 정보 수정 요청
       MvcResult updateResult = mockMvc
           .perform(put("/user")
