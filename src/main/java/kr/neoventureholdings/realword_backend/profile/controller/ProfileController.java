98389d989c7363f9187d64b94983549fdd1122bd
이민기 <mon0mon@naver.com>
2024-09-17 10:20:21 +0900

    ✨ feat : Profile - Profile Controller 구현 - Profile Service 구현 - ProfileResponseDto에 Following 항목 값 추가

--- src/main/java/kr/neoventureholdings/realword_backend/profile/controller/ProfileController.java
+++ src/main/java/kr/neoventureholdings/realword_backend/profile/controller/ProfileController.java
@@ -1,6 +1,7 @@
 package kr.neoventureholdings.realword_backend.profile.controller;
 
 import kr.neoventureholdings.realword_backend.config.security.authentication.CustomUserDetail;
+import kr.neoventureholdings.realword_backend.profile.dto.ProfileResponseDto;
 import kr.neoventureholdings.realword_backend.profile.service.FacadeProfileService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,45 +19,51 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/profiles")
 public class ProfileController {
+
   private final FacadeProfileService profileService;
 
   /**
    * 주어진 username에 해당하는 이용자의 프로필을 조회
+   *
    * @param username
    * @return
    */
   @GetMapping("/{username}")
-  public ResponseEntity<String> getProfile(@PathVariable("username") String username, @AuthenticationPrincipal CustomUserDetail customUserDetail) {
+  public ResponseEntity<ProfileResponseDto> getProfile(@PathVariable("username") String username,
+      @AuthenticationPrincipal CustomUserDetail customUserDetail) {
     return ResponseEntity
         .ok()
-        .body(null);
+        .body(profileService.getProfile(username, customUserDetail));
   }
 
   /**
    * 주어진 username에 해당하는 이용자를 follow
+   *
    * @param username
    * @param customUserDetail
    * @return
    */
   @PostMapping("/{username}/follow")
-  public ResponseEntity<String> followProfile(@PathVariable("username") String username,
+  public ResponseEntity<ProfileResponseDto> followProfile(@PathVariable("username") String username,
       @AuthenticationPrincipal CustomUserDetail customUserDetail) {
     return ResponseEntity
         .ok()
-        .body(null);
+        .body(profileService.followUser(username, customUserDetail));
   }
 
   /**
    * 주어진 username에 해당하는 이용자를 unfollow
+   *
    * @param username
    * @param customUserDetail
    * @return
    */
   @DeleteMapping("/{username}/follow")
-  public ResponseEntity<String> unfollowProfile(@PathVariable("username") String username,
+  public ResponseEntity<ProfileResponseDto> unfollowProfile(
+      @PathVariable("username") String username,
       @AuthenticationPrincipal CustomUserDetail customUserDetail) {
     return ResponseEntity
         .ok()
-        .body(null);
+        .body(profileService.unfollowUser(username, customUserDetail));
   }
 }
