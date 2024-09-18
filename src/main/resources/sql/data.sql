481d11ad4b48ec4591ecd34eb795bf270a01a19f
이민기 <mon0mon@naver.com>
2024-09-18 11:41:01 +0900

    🛠️ project : Initial Data - 초기 데이터 수정

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -14,26 +14,32 @@ VALUES ((select id from users where email like 'celeb@example.com'), 'celeb', 'J
         'https://simpleicons.org/icons/openjdk.svg');
 
 -- Article 조회용 정보 추가
-INSERT INTO article (author_id, title, description, body)
+INSERT INTO article (author_id, title, description, body, slug, created_at, updated_at)
 values ((select id from users where email like 'celeb@example.com'), 'Java EE', 'Java Enterprise Edition',
-        'Java Platform, Enterprise Edition (Java EE) is the standard in community-driven enterprise software.');
+        'Java Platform, Enterprise Edition (Java EE) is the standard in community-driven enterprise software.',
+        random_uuid(), current_timestamp, current_timestamp);
 
-INSERT INTO article (author_id, title, description, body)
+INSERT INTO article (author_id, title, description, body, slug, created_at, updated_at)
 values ((select id from users where email like 'celeb@example.com'), 'Spring', 'Spring Framework',
-        'The Spring Framework is an application framework and inversion of control container for the Java platform.');
+        'The Spring Framework is an application framework and inversion of control container for the Java platform.',
+        random_uuid(), current_timestamp, current_timestamp);
 
-INSERT INTO article (author_id, title, description, body)
+INSERT INTO article (author_id, title, description, body, slug, created_at, updated_at)
 values ((select id from users where email like 'celeb@example.com'), 'Node.js', 'Node.js',
-    'Node.js is a cross-platform, open-source JavaScript runtime environment.');
+    'Node.js is a cross-platform, open-source JavaScript runtime environment.',
+        random_uuid(), current_timestamp, current_timestamp);
 
-INSERT INTO article (author_id, title, description, body)
+INSERT INTO article (author_id, title, description, body, slug, created_at, updated_at)
 values ((select id from users where email like 'celeb@example.com'), 'Next.js', 'The React Framework',
-    'Open-source web development framework React-based web applications with server-side rendering and static website generation.');
+    'Open-source web development framework React-based web applications with server-side rendering and static website generation.',
+        random_uuid(), current_timestamp, current_timestamp);
 
-INSERT INTO article (author_id, title, description, body)
+INSERT INTO article (author_id, title, description, body, slug, created_at, updated_at)
 values ((select id from users where email like 'celeb@example.com'), 'Struts', 'Apache Struts',
-    'Apache Struts is a free, open-source, MVC framework for creating elegant, modern Java web applications.');
+    'Apache Struts is a free, open-source, MVC framework for creating elegant, modern Java web applications.',
+        random_uuid(), current_timestamp, current_timestamp);
 
-INSERT INTO article (author_id, title, description, body)
+INSERT INTO article (author_id, title, description, body, slug, created_at, updated_at)
 values ((select id from users where email like 'celeb@example.com'), 'ASP.NET', 'ASP.NET',
-    'ASP.NET is a server-side web-application framework designed for web development to produce dynamic web pages.');
+    'ASP.NET is a server-side web-application framework designed for web development to produce dynamic web pages.',
+        random_uuid(), current_timestamp, current_timestamp);
