8353082ab09e99eba2df012781a577a260d65cac
이민기 <mon0mon@naver.com>
2024-09-18 20:19:55 +0900

    ✨ feat : Article - 게시글 목록 검색 간 필터링 옵션 적용 (JPA Criteria API 사용)
    🐛 bug : Article
    - 게시글의 title 업데이트 할 경우 slug unique 체크하는 로직 추가
    
    📝 docs : Article
    - 게시글 목록 조회와 관련된 내용 정리

--- src/main/java/kr/neoventureholdings/realword_backend/article/service/ArticleService.java
+++ src/main/java/kr/neoventureholdings/realword_backend/article/service/ArticleService.java
@@ -1,19 +1,28 @@
 package kr.neoventureholdings.realword_backend.article.service;
 
-import java.util.List;
+import java.util.Optional;
 import kr.neoventureholdings.realword_backend.article.domains.Article;
 import kr.neoventureholdings.realword_backend.article.dto.ArticleDto;
 import kr.neoventureholdings.realword_backend.article.dto.ArticleRequestDto;
 import kr.neoventureholdings.realword_backend.article.dto.ArticleRequestParamDto;
 import kr.neoventureholdings.realword_backend.article.repository.ArticleRepository;
+import kr.neoventureholdings.realword_backend.article.repository.ArticleSpecification;
 import kr.neoventureholdings.realword_backend.auth.domains.User;
 import kr.neoventureholdings.realword_backend.auth.service.FacadeUserService;
 import kr.neoventureholdings.realword_backend.config.security.authentication.CustomUserDetail;
 import kr.neoventureholdings.realword_backend.exception.auth.NoAuthorizationException;
 import kr.neoventureholdings.realword_backend.exception.common.NoSuchElementException;
+import kr.neoventureholdings.realword_backend.exception.common.UniqueConstraintViolationException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
@@ -27,8 +36,11 @@ public class ArticleService {
   }
 
   //  TODO 필터링 옵션을 어떻게 적용할 것인지 고민해보고 구현하기
-  public List<Article> getArticles(ArticleRequestParamDto paramDto) {
-    return null;
+  public Page<Article> getArticles(ArticleRequestParamDto paramDto) {
+    Pageable pageable = PageRequest.of(paramDto.getOffset(), paramDto.getLimit(),
+        Sort.by(Direction.DESC, "id"));
+    Specification<Article> specification = ArticleSpecification.getArticlesByFilters(paramDto);
+    return articleRepository.findAll(specification, pageable);
   }
 
   public Article saveArticle(ArticleRequestDto articleRequestDto, CustomUserDetail userDetail) {
@@ -39,8 +51,12 @@ public class ArticleService {
     return articleRepository.save(article);
   }
 
+  @Transactional
   public Article updateArticle(ArticleRequestDto articleRequestDto, String slug, CustomUserDetail userDetail) {
     User user = facadeUserService.getCurrentUser(userDetail);
+
+    ArticleDto articleDto = ArticleDto.of(articleRequestDto);
+    checkSlugIsUnique(slug, articleDto.getSlug());
 
     Article article = getArticleBySlug(slug);
 
@@ -48,11 +64,12 @@ public class ArticleService {
       throw new NoAuthorizationException("can't update article. you are not a author");
     }
 
-    article = article.of(ArticleDto.of(articleRequestDto), false);
+    article = article.of(articleDto);
 
     return articleRepository.save(article);
   }
 
+  @Transactional
   public void deleteArticle(String slug, CustomUserDetail userDetail) {
     User user = facadeUserService.getCurrentUser(userDetail);
 
@@ -79,5 +96,16 @@ public class ArticleService {
     ArticleDto articleDto = ArticleDto.of(articleRequestDto);
     Article article = Article.of(articleDto, user);
     return article;
+  }
+
+  private void checkSlugIsUnique(String oldSlug, String newSlug) {
+    if (oldSlug.equals(newSlug)) {
+      return;
+    }
+
+    Optional<Article> articleBySlug = articleRepository.findArticleBySlug(newSlug);
+    articleBySlug.ifPresent(value -> {
+      throw new UniqueConstraintViolationException("slug [" + newSlug + "] is Already Exists");
+    });
   }
 }
