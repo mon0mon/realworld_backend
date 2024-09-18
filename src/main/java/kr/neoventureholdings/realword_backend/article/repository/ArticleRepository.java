eb0b2dcfaafdd5cbc4559a332d7ec3e6959eccea
이민기 <mon0mon@naver.com>
2024-09-18 23:45:59 +0900

    ✨ feat : Favorite - User - Favorite - Article 연관관계 매핑 (1 : N, M : 1)

--- src/main/java/kr/neoventureholdings/realword_backend/article/repository/ArticleRepository.java
+++ src/main/java/kr/neoventureholdings/realword_backend/article/repository/ArticleRepository.java
@@ -3,6 +3,9 @@ package kr.neoventureholdings.realword_backend.article.repository;
 import java.util.Optional;
 import kr.neoventureholdings.realword_backend.article.domains.Article;
 import kr.neoventureholdings.realword_backend.auth.domains.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
@@ -23,4 +26,7 @@ public interface ArticleRepository extends JpaRepository<Article, Long>,
 
   @EntityGraph(value = "Article.withUser")
   public Optional<Article> findArticleByAuthorAndSlug(User author, String slug);
+
+  @EntityGraph(value = "Article.withUserAndFavorites")
+  public Page<Article> findAll(Specification<Article> spec, Pageable pageable);
 }
