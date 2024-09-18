e1804769b1fc5c6ae9f3524ad272f7590c1a7001
이민기 <mon0mon@naver.com>
2024-09-19 00:48:30 +0900

    ✨ feat : Favorite - Favorite 비즈니스 로직 추가 - Favorite DTO 추가

---
+++ src/main/java/kr/neoventureholdings/realword_backend/favorite/service/FavoriteService.java
@@ -0,0 +1,31 @@
+package kr.neoventureholdings.realword_backend.favorite.service;
+
+import kr.neoventureholdings.realword_backend.favorite.domains.Favorite;
+import kr.neoventureholdings.realword_backend.favorite.dto.FavoriteDto;
+import kr.neoventureholdings.realword_backend.favorite.repository.FavoriteRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class FavoriteService {
+  private final FavoriteRepository favoriteRepository;
+
+  @Transactional
+  public Favorite saveFavorite(FavoriteDto dto) {
+    return favoriteRepository.save(Favorite.of(dto));
+  }
+
+  @Transactional
+  public void deleteFavorite(FavoriteDto dto) {
+    deleteFavorite(Favorite.of(dto));
+  }
+
+  @Transactional
+  public void deleteFavorite(Favorite favorite) {
+    favoriteRepository.delete(favorite);
+  }
+}
