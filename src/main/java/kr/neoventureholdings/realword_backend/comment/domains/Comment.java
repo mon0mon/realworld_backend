c065bb1851fe4bf9c4e6a39875ec1cc1baab71f0
이민기 <mon0mon@naver.com>
2024-09-22 00:32:26 +0900

    ✨ feat : Comment - Comment 단일 조회, 목록 조회, 삭제 기능 구현

--- src/main/java/kr/neoventureholdings/realword_backend/comment/domains/Comment.java
+++ src/main/java/kr/neoventureholdings/realword_backend/comment/domains/Comment.java
@@ -1,10 +1,15 @@
 package kr.neoventureholdings.realword_backend.comment.domains;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.NamedAttributeNode;
+import jakarta.persistence.NamedEntityGraph;
+import jakarta.persistence.NamedSubgraph;
 import jakarta.persistence.Table;
 import kr.neoventureholdings.realword_backend.article.domains.Article;
 import kr.neoventureholdings.realword_backend.auth.domains.User;
@@ -29,9 +34,11 @@ public class Comment extends BaseEntity {
 
   private String body;
 
-  @ManyToOne
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id")
   private User user;
 
-  @ManyToOne
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "article_id")
   private Article article;
 }
