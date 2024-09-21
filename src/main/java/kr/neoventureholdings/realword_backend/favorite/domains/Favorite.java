8f7997098665bdf8776e58ed767f8c885985d51d
이민기 <mon0mon@naver.com>
2024-09-21 19:54:04 +0900

    ♻️ refactor : Article, Tag, Profile - Article, Tag의 연관관계 수정 - Article 목록조회간 Tag, Profile의 N+1 문제 수정

--- src/main/java/kr/neoventureholdings/realword_backend/favorite/domains/Favorite.java
+++ src/main/java/kr/neoventureholdings/realword_backend/favorite/domains/Favorite.java
@@ -1,6 +1,7 @@
 package kr.neoventureholdings.realword_backend.favorite.domains;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -28,10 +29,10 @@ public class Favorite {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @ManyToOne
+  @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "user_id")
   private User user;
-  @ManyToOne
+  @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "article_id")
   private Article article;
 
