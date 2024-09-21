d4e72003f676d2187d92250c078e68f99fdb9c01
이민기 <mon0mon@naver.com>
2024-09-21 13:40:05 +0900

    ✨ feat : Tag - Tag DTO 추가
    ♻️ refactor : Tag
    - Tag Map Repository 메소드 정보 수정
    - Tag Map 비교 연산자 추가

--- src/main/java/kr/neoventureholdings/realword_backend/tag/repository/TagMapRepository.java
+++ src/main/java/kr/neoventureholdings/realword_backend/tag/repository/TagMapRepository.java
@@ -1,5 +1,6 @@
 package kr.neoventureholdings.realword_backend.tag.repository;
 
+import java.util.Optional;
 import java.util.Set;
 import kr.neoventureholdings.realword_backend.article.domains.Article;
 import kr.neoventureholdings.realword_backend.tag.domains.Tag;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TagMapRepository extends JpaRepository<TagMap, Long> {
-  public Set<Tag> findAllByArticle(Article article);
-  public Set<Article> findAllByTag(Tag tag);
+  public Set<TagMap> findAllByTag(Tag tag);
+  public Set<TagMap> findAllByArticle(Article article);
+  public Optional<TagMap> findByArticleAndTag(Article article, Tag tag);
 }
