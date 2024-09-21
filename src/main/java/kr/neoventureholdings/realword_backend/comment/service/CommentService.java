098ebcbf5a9a9385fa2563177ac7dc8e324f2918
이민기 <mon0mon@naver.com>
2024-09-22 00:32:26 +0900

    ✨ feat : Comment - Comment 단일 조회, 목록 조회, 삭제 기능 구현

--- src/main/java/kr/neoventureholdings/realword_backend/comment/service/CommentService.java
+++ src/main/java/kr/neoventureholdings/realword_backend/comment/service/CommentService.java
@@ -1,8 +1,16 @@
 package kr.neoventureholdings.realword_backend.comment.service;
 
+import java.util.List;
+import kr.neoventureholdings.realword_backend.article.domains.Article;
+import kr.neoventureholdings.realword_backend.article.service.FacadeArticleService;
+import kr.neoventureholdings.realword_backend.auth.domains.User;
+import kr.neoventureholdings.realword_backend.auth.service.FacadeUserService;
 import kr.neoventureholdings.realword_backend.comment.domains.Comment;
 import kr.neoventureholdings.realword_backend.comment.dto.CommentRequestDto;
 import kr.neoventureholdings.realword_backend.comment.repository.CommentRepository;
+import kr.neoventureholdings.realword_backend.exception.auth.NoAuthorizationException;
+import kr.neoventureholdings.realword_backend.exception.common.NoSuchElementException;
+import kr.neoventureholdings.realword_backend.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -12,12 +20,39 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CommentService {
   private final CommentRepository commentRepository;
+  private final FacadeUserService facadeUserService;
+  private final FacadeArticleService facadeArticleService;
+
+  public List<Comment> getComments(String slug) {
+    Article article = facadeArticleService.getArticle(slug);
+    List<Comment> commentList = commentRepository.findAllByArticle(article);
+    return commentList;
+  }
 
   public Comment saveComment(String slug, CommentRequestDto requestDto) {
-    return null;
+    User user = facadeUserService.getCurrentUser(SecurityUtil.getCurrentUserCustomUserdetail());
+    Article article = facadeArticleService.getArticle(slug);
+
+    Comment comment = Comment.builder()
+        .body(requestDto.getBody())
+        .article(article)
+        .user(user)
+        .build();
+
+    return commentRepository.save(comment);
   }
 
   public void deleteComment(String slug, Long commentId) {
+    User user = facadeUserService.getCurrentUser(SecurityUtil.getCurrentUserCustomUserdetail());
+    Article article = facadeArticleService.getArticle(slug);
 
+    Comment comment = commentRepository.findById(commentId)
+        .orElseThrow(() -> new NoSuchElementException("no such comment"));
+
+    if (!comment.getUser().equals(user)) {
+      throw new NoAuthorizationException("no authorization to delete comment");
+    }
+
+    commentRepository.delete(comment);
   }
 }
--- src/main/java/kr/neoventureholdings/realword_backend/comment/service/FacadeCommentService.java
+++ src/main/java/kr/neoventureholdings/realword_backend/comment/service/FacadeCommentService.java
@@ -1,5 +1,6 @@
 package kr.neoventureholdings.realword_backend.comment.service;
 
+import java.util.List;
 import kr.neoventureholdings.realword_backend.comment.domains.Comment;
 import kr.neoventureholdings.realword_backend.comment.dto.CommentRequestDto;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +12,10 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class FacadeCommentService {
   private final CommentService commentService;
+
+  public List<Comment> getComments(String slug) {
+    return commentService.getComments(slug);
+  }
 
   public Comment createComment(String slug, CommentRequestDto requestDto) {
     return commentService.saveComment(slug, requestDto);
