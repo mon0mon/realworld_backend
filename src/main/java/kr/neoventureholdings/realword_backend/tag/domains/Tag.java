65d824d7fd27589db3a5cfc6e564daff6d4b3566
이민기 <mon0mon@naver.com>
2024-09-21 13:41:41 +0900

    🐛 bug : Article, Tag - Article - Tag 연관관계 매핑이 잘못되어있던 문제 수정

--- src/main/java/kr/neoventureholdings/realword_backend/tag/domains/Tag.java
+++ src/main/java/kr/neoventureholdings/realword_backend/tag/domains/Tag.java
@@ -1,17 +1,20 @@
 package kr.neoventureholdings.realword_backend.tag.domains;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.util.HashSet;
 import java.util.Set;
+import kr.neoventureholdings.realword_backend.article.domains.Article;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -24,21 +27,22 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "tag")
+@Table(name = "tag", indexes = {
+    @Index(name = "idx_tag_value", columnList = "tag_value")
+})
 public class Tag {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
-  @Column(name = "tag_value")
+  @Column(name = "tag_value", unique = true)
   private String value;
 
   //  현재 태그가 포함되어있는 게시글 조회
+  @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST, orphanRemoval = true)
   @JoinTable(
       name = "tag_map",
       joinColumns = @JoinColumn(name = "tag_id"),
       inverseJoinColumns = @JoinColumn(name = "article_id")
   )
-  @OneToMany(fetch = FetchType.EAGER)
-  @Builder.Default
-  private Set<TagMap> articles = new HashSet<>();
+  private Set<Article> articles = new HashSet<>();
 }
