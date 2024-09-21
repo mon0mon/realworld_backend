0b8c29ecec805f4bc00a11ed50b4ab498b7d489c
이민기 <mon0mon@naver.com>
2024-09-21 13:45:00 +0900

    🛠️ project : Initial Data - 초기 데이터 수정

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -16,7 +16,8 @@ VALUES ((select id from users where email like 'celeb@example.com'), 'celeb', 'J
 -- Article 조회용 정보 추가
 -- 1
 INSERT INTO article (author_id, title, description, body, slug, created_at, updated_at)
-values ((select id from users where email like 'celeb@example.com'), 'Java EE', 'Java Enterprise Edition',
+values ((select id from users where email like 'celeb@example.com'), 'Java EE',
+        'Java Enterprise Edition',
         'Java Platform, Enterprise Edition (Java EE) is the standard in community-driven enterprise software.',
         'java-ee', current_timestamp, current_timestamp);
 
@@ -29,25 +30,26 @@ values ((select id from users where email like 'celeb@example.com'), 'Spring', '
 -- 3
 INSERT INTO article (author_id, title, description, body, slug, created_at, updated_at)
 values ((select id from users where email like 'celeb@example.com'), 'Node.js', 'Node.js',
-    'Node.js is a cross-platform, open-source JavaScript runtime environment.',
+        'Node.js is a cross-platform, open-source JavaScript runtime environment.',
         'node-js', current_timestamp, current_timestamp);
 
 -- 4
 INSERT INTO article (author_id, title, description, body, slug, created_at, updated_at)
-values ((select id from users where email like 'celeb@example.com'), 'Next.js', 'The React Framework',
-    'Open-source web development framework React-based web applications with server-side rendering and static website generation.',
+values ((select id from users where email like 'celeb@example.com'), 'Next.js',
+        'The React Framework',
+        'Open-source web development framework React-based web applications with server-side rendering and static website generation.',
         'next-js', current_timestamp, current_timestamp);
 
 -- 5
 INSERT INTO article (author_id, title, description, body, slug, created_at, updated_at)
 values ((select id from users where email like 'celeb@example.com'), 'Struts', 'Apache Struts',
-    'Apache Struts is a free, open-source, MVC framework for creating elegant, modern Java web applications.',
+        'Apache Struts is a free, open-source, MVC framework for creating elegant, modern Java web applications.',
         'apache-struts', current_timestamp, current_timestamp);
 
 -- 6
 INSERT INTO article (author_id, title, description, body, slug, created_at, updated_at)
 values ((select id from users where email like 'celeb@example.com'), 'ASP.NET', 'ASP.NET',
-    'ASP.NET is a server-side web-application framework designed for web development to produce dynamic web pages.',
+        'ASP.NET is a server-side web-application framework designed for web development to produce dynamic web pages.',
         'asp-net', current_timestamp, current_timestamp);
 
 -- 1
@@ -60,7 +62,7 @@ values ((select id from users where email like 'user1@example.com'), 'C', 'C',
 INSERT INTO article (author_id, title, description, body, slug, created_at, updated_at)
 values ((select id from users where email like 'user1@example.com'), 'C++', 'CPP',
         'C++ is a high-level, general-purpose programming language',
-        'cpp', current_timestamp, current_timestamp);
+        'c--', current_timestamp, current_timestamp);
 
 -- 3
 INSERT INTO article (author_id, title, description, body, slug, created_at, updated_at)
@@ -79,3 +81,50 @@ INSERT INTO article (author_id, title, description, body, slug, created_at, upda
 values ((select id from users where email like 'user1@example.com'), 'JavaScript', 'js',
         'JavaScript is a programming language and core technology of the Web',
         'javascript', current_timestamp, current_timestamp);
+
+-- Tag 정보 추가
+insert into tag (tag_value)
+values ('Language');
+
+insert into tag (tag_value)
+values ('Web Framework');
+
+insert into tag (tag_value)
+values ('Java');
+
+insert into tag (tag_value)
+values ('C Family');
+
+insert into tag (tag_value)
+values ('Javascript');
+
+-- 태그와 게시글 매핑 정보 추가
+insert into tag_map (article_id, tag_id)
+select a.id, t.id
+from article a
+         join tag t on t.tag_value = 'Web Framework'
+where a.slug in ('java-ee', 'spring', 'node-js', 'next-js', 'apache-struts', 'asp-net');
+
+insert into tag_map (article_id, tag_id)
+select a.id, t.id
+from article a
+         join tag t on t.tag_value = 'Language'
+where a.slug in ('c', 'c--', 'c-', 'java', 'javascript');
+
+insert into tag_map (article_id, tag_id)
+select a.id, t.id
+from article a
+         join tag t on t.tag_value = 'Java'
+where a.slug in ('java-ee', 'spring', 'apache-struts', 'java');
+
+insert into tag_map (article_id, tag_id)
+select a.id, t.id
+from article a
+         join tag t on t.tag_value = 'C Family'
+where a.slug in ('asp-net', 'c', 'c--', 'c-');
+
+insert into tag_map (article_id, tag_id)
+select a.id, t.id
+from article a
+         join tag t on t.tag_value = 'Javascript'
+where a.slug in ('javascript', 'node-js', 'next-js');
