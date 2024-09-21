613585f5f750d79900766c7b08a84de18f9b188a
이민기 <mon0mon@naver.com>
2024-09-21 19:27:53 +0900

    🧪 test : Feed - Feed Controller 테스트 추가

---
+++ src/test/java/kr/neoventureholdings/realword_backend/feed/FeedTestConstant.java
@@ -0,0 +1,5 @@
+package kr.neoventureholdings.realword_backend.feed;
+
+public class FeedTestConstant {
+  public static final String FEED_FOLLOWEE_USERNAME = "celeb";
+}
---
+++ src/test/java/kr/neoventureholdings/realword_backend/feed/controller/FeedControllerTest.java
@@ -0,0 +1,77 @@
+package kr.neoventureholdings.realword_backend.feed.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kr.neoventureholdings.realword_backend.auth.AuthTestConstant;
+import kr.neoventureholdings.realword_backend.config.security.authentication.CustomUserDetail;
+import kr.neoventureholdings.realword_backend.constant.TokenConstant;
+import kr.neoventureholdings.realword_backend.feed.FeedTestConstant;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public class FeedControllerTest {
+  @Autowired
+  private MockMvc mockMvc;
+  private static final ObjectMapper objectMapper = new ObjectMapper();
+
+  @BeforeAll
+  static void beforeAll() {
+    objectMapper.setSerializationInclusion(Include.NON_NULL);
+  }
+
+  @Test
+  @DisplayName("게시글 컨트롤러 테스트 - 목록 조회 (로그인 X)(Filter : Author)(Author : celeb)")
+  @PreAuthorize("isAuthenticated()")
+  @WithUserDetails(value = AuthTestConstant.EMAIL, userDetailsServiceBeanName = "testUserDetailService")
+  void getArticleListByAuthor() {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    CustomUserDetail userDetails = (CustomUserDetail) authentication.getPrincipal();
+
+    String token = userDetails.getToken();
+
+    try {
+      mockMvc.perform(get("/articles/feed")
+              .header(HttpHeaders.AUTHORIZATION, TokenConstant.TOKEN_HEADER_PREFIX + token)
+              .contentType(MediaType.APPLICATION_JSON_VALUE)
+          )
+          .andDo(print())
+          .andExpectAll(
+              status().isOk(),
+              content().contentType(MediaType.APPLICATION_JSON),
+              jsonPath("$.articlesCount").value(6),
+              jsonPath("$.articles[*].author.username")
+                  .value(Matchers.everyItem(
+                      Matchers.equalTo(FeedTestConstant.FEED_FOLLOWEE_USERNAME))
+                  )
+          );
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
