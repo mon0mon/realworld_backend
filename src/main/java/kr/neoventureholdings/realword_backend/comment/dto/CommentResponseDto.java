21ee8d613dfe166d3ee92856d16521c29936d242
이민기 <mon0mon@naver.com>
2024-09-22 09:29:49 +0900

    ✨ feat : Comment - 댓글 응답값에 following 여부 추가

--- src/main/java/kr/neoventureholdings/realword_backend/comment/dto/CommentResponseDto.java
+++ src/main/java/kr/neoventureholdings/realword_backend/comment/dto/CommentResponseDto.java
@@ -2,8 +2,9 @@ package kr.neoventureholdings.realword_backend.comment.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalDateTime;
-import kr.neoventureholdings.realword_backend.auth.dto.UserResponseDto;
+import kr.neoventureholdings.realword_backend.auth.domains.User;
 import kr.neoventureholdings.realword_backend.comment.domains.Comment;
+import kr.neoventureholdings.realword_backend.profile.dto.ProfileResponseDto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,15 +20,15 @@ public class CommentResponseDto {
   private Long id;
   private String body;
   @JsonProperty("author")
-  private UserResponseDto author;
+  private ProfileResponseDto author;
   private LocalDateTime createAt;
   private LocalDateTime updatedAt;
 
-  public static CommentResponseDto of(Comment comment) {
+  public static CommentResponseDto of(Comment comment, User user) {
     return CommentResponseDto.builder()
         .id(comment.getId())
         .body(comment.getBody())
-        .author(UserResponseDto.of(comment.getUser()))
+        .author(comment.getUser().getProfile().of(user))
         .createAt(comment.getCreatedAt())
         .updatedAt(comment.getUpdatedAt())
         .build();
